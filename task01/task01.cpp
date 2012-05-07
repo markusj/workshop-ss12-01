868b7baa1015241376a1251d24a48689d618b8f1
Markus Jung <markus@markusnb>
2012-05-07 19:05:52 +0100

    Add some more ways to print my name by using iterators

--- task01/task01.cpp
+++ task01/task01.cpp
@@ -1,16 +1,25 @@
+#include <string>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    const string nameReversed = "sukraM";
+    string nameReversed = "sukraM";
 
     for (size_t i = 0; i < nameReversed.length(); i++) {
         cout << nameReversed.at(nameReversed.length() - i - 1);
     }
 
+    cout << endl;
+
+    for (string::reverse_iterator iter = nameReversed.rbegin(); iter < nameReversed.rend(); iter++) {
+        cout << *iter;
+    }
+
 	cout << endl;
+
+	cout << string(nameReversed.rbegin(), nameReversed.rend()) << endl;
 
 	return 0;
 }
